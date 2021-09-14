6256b08e4c5ae0bcf532e847820a56556ef36a28
Rudra Saraswat <rs2009@ubuntu.com>
2021-09-14 18:03:18 +0530

    Edit GRUB config

--- build/remix.sh
+++ build/remix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 add-apt-repository -y --no-update universe
-add-apt-repository -y --no-update multiverse
+add-apt-repository -y multiverse
 
 add-apt-repository -y ppa:ubuntucinnamonremix/unstable
 
@@ -10,3 +10,10 @@ apt-get install -y casper expect gparted ubuntucinnamon-desktop \
  xfsprogs jfsutils reiserfsprogs shim-signed shim ntfs-3g lvm2 \
  dmraid wamerican fwupdate mokutil \
  zram-config ubiquity ubiquity-slideshow-ubuntu ubiquity-frontend-gtk
+
+apt-get purge -y unity-greeter
+
+cat > /usr/share/glib-2.0/schemas/90_ubuntucinnamon-wallpaper.gschema.override <<EOF
+[org.gnome.desktop.background]
+picture-uri='file:///usr/share/backgrounds/ubuntucinnamon/Ubuntu_Cinnamon_Ubuntu.jpg'
+EOF
