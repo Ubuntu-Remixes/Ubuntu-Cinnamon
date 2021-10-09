f0101ccbd74f8b8699850d7ed2e33ffe578e692a
Rudra Saraswat <rs2009@ubuntu.com>
2021-10-09 11:27:28 +0530

    Fix Ubiquity background issues

--- build/remix.sh
+++ build/remix.sh
@@ -8,14 +8,16 @@ add-apt-repository -y ppa:ubuntucinnamonremix/unstable
 apt-get install -y casper expect gparted ubuntucinnamon-desktop \
  cifs-utils lupin-casper language-pack-en language-pack-gnome-en \
  xfsprogs jfsutils reiserfsprogs shim-signed shim ntfs-3g lvm2 \
- dmraid wamerican fwupdate mokutil \
+ dmraid wamerican fwupdate mokutil ubuntu-settings \
  zram-config ubiquity ubiquity-slideshow-ubuntu ubiquity-frontend-gtk
 
-apt-get purge -y unity-greeter
+apt-get install -y gnome-shell --no-install-recommends && apt-get purge -y gnome-session ubuntu-session gdm3 unity-greeter metacity
 
 cat > /usr/share/glib-2.0/schemas/90_ubuntucinnamon-wallpaper.gschema.override <<EOF
 [org.gnome.desktop.background]
-picture-uri='file:///usr/share/backgrounds/ubuntucinnamon/Ubuntu_Cinnamon_Ubuntu.jpg'
+picture-uri='file:///usr/share/backgrounds/ubuntucinnamon/impish/ubuntu_cinnamon_impish_indri.jpg'
 EOF
 
 glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+apt-get purge -y budgie-core --auto-remove
