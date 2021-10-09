89d9b0f4feea2598c834f6f6ca7c67fe3532b014
Joshua Peisach <itzswirlz2020@outlook.com>
2021-10-09 11:58:57 -0400

    Switch repos for 21.10 release

--- build/remix.sh
+++ build/remix.sh
@@ -3,7 +3,7 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-add-apt-repository -y ppa:ubuntucinnamonremix/unstable
+add-apt-repository -y ppa:ubuntucinnamonremix/all
 
 apt-get install -y casper expect gparted ubuntucinnamon-desktop \
  cifs-utils lupin-casper language-pack-en language-pack-gnome-en \
