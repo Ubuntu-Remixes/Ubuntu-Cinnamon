e078df94ccf5a6e8ecc1865fbc8dce255f819266
Joshua Peisach <itzswirlz2020@outlook.com>
2021-10-23 12:53:57 -0400

    Switch ppas

--- build/remix.sh
+++ build/remix.sh
@@ -3,7 +3,7 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-add-apt-repository -y ppa:ubuntucinnamonremix/all
+add-apt-repository -y ppa:ubuntucinnamonremix/unstable
 
 apt-get install -y casper expect gparted ubuntucinnamon-desktop \
  cifs-utils lupin-casper language-pack-en language-pack-gnome-en \
