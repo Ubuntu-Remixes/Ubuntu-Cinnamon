3e640f03565bb790a5330449827f91de0303ffbd
Joshua Peisach <itzswirlz2020@outlook.com>
2022-03-19 14:26:44 -0400

    build: don't install lutpin-casper
    Package doesn't exist, causes build to skip installing
    the UCR environment

--- build/remix.sh
+++ build/remix.sh
@@ -6,7 +6,7 @@ add-apt-repository -y multiverse
 add-apt-repository -y ppa:ubuntucinnamonremix/unstable
 
 apt-get install -y casper expect gparted ubuntucinnamon-desktop \
- cifs-utils lupin-casper language-pack-en language-pack-gnome-en \
+ cifs-utils language-pack-en language-pack-gnome-en \
  xfsprogs jfsutils reiserfsprogs shim-signed shim ntfs-3g lvm2 \
  dmraid wamerican fwupdate mokutil ubuntu-settings \
  zram-config ubiquity ubiquity-slideshow-ubuntu ubiquity-frontend-gtk
