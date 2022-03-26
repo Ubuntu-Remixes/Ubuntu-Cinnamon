736f0bc094268acea20ba7c36b38908f3578b709
Joshua Peisach <itzswirlz2020@outlook.com>
2022-03-26 09:37:42 -0400

    Update build/remix.sh
    Update wallpaper, switch from zram-config to zram-tools

--- build/remix.sh
+++ build/remix.sh
@@ -9,13 +9,13 @@ apt-get install -y casper expect gparted ubuntucinnamon-desktop \
  cifs-utils language-pack-en language-pack-gnome-en \
  xfsprogs jfsutils reiserfsprogs shim-signed shim ntfs-3g lvm2 \
  dmraid wamerican fwupdate mokutil ubuntu-settings \
- zram-config ubiquity ubiquity-slideshow-ubuntu ubiquity-frontend-gtk
+ zram-tools ubiquity ubiquity-slideshow-ubuntu ubiquity-frontend-gtk
 
 apt-get install -y gnome-shell --no-install-recommends && apt-get purge -y gnome-session ubuntu-session gdm3 unity-greeter metacity
 
 cat > /usr/share/glib-2.0/schemas/90_ubuntucinnamon-wallpaper.gschema.override <<EOF
 [org.gnome.desktop.background]
-picture-uri='file:///usr/share/backgrounds/ubuntucinnamon/impish/ubuntu_cinnamon_impish_indri.jpg'
+picture-uri='file:///usr/share/backgrounds/ubuntucinnamon/jammy/ubuntu_cinnamon_jammy_jellyfish.jpg'
 EOF
 
 glib-compile-schemas /usr/share/glib-2.0/schemas/
