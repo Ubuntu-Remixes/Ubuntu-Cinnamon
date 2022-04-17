7818212b1e5cb6333bf242683048d89d90553a80
Joshua Peisach <itzswirlz2020@outlook.com>
2022-04-17 11:15:27 -0400

    Prepare/config for RC

--- build/remix.sh
+++ build/remix.sh
@@ -3,7 +3,7 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-add-apt-repository -y ppa:ubuntucinnamonremix/unstable
+add-apt-repository -y ppa:ubuntucinnamonremix/all
 
 apt-get install -y casper expect gparted ubuntucinnamon-desktop \
  cifs-utils language-pack-en language-pack-gnome-en \
@@ -18,6 +18,12 @@ cat > /usr/share/glib-2.0/schemas/90_ubuntucinnamon-wallpaper.gschema.override <
 picture-uri='file:///usr/share/backgrounds/ubuntucinnamon/jammy/ubuntu_cinnamon_jammy_jellyfish.jpg'
 EOF
 
+apt-get install -y gdm3 metacity
+
 glib-compile-schemas /usr/share/glib-2.0/schemas/
 
 apt-get purge -y budgie-core --auto-remove
+
+apt-get -y autoremove
+apt-get -y clean
+apt-get -y autoclean
